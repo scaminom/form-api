cfd55d745f391a6b67000daae5db2143c83cf959
JMSebas <jmoreira8505@uta.edu.ec>
2023-12-04 17:17:26 -0500

    Creacion modelo de roles

---
+++ db/migrate/20231204221608_create_roles.rb
@@ -0,0 +1,11 @@
+class CreateRoles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :roles do |t|
+      t.string :name
+      t.string :descripcion
+      t.integer :priority
+
+      t.timestamps
+    end
+  end
+end
